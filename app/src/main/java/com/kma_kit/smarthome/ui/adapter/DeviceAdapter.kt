012aaa7f3886bc58b847a5a1c90d7e8d30b6b859
vu_nd <nguyendangvu221@gmail.com>
2024-06-18 17:26:45 +0700

    add style for UI

--- app/src/main/java/com/kma_kit/smarthome/ui/adapter/DeviceAdapter.kt
+++ app/src/main/java/com/kma_kit/smarthome/ui/adapter/DeviceAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.kma_kit.smarthome.R
 import com.kma_kit.smarthome.data.entity.DeviceTabbar
@@ -14,7 +15,7 @@ class DeviceAdapter(private val devices: List<DeviceTabbar>) : RecyclerView.Adap
     class DeviceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val deviceName: TextView = itemView.findViewById(R.id.deviceName)
         val deviceType: TextView = itemView.findViewById(R.id.deviceType)
-        val deviceSwitch: Switch = itemView.findViewById(R.id.deviceSwitch)
+        val deviceSwitch: SwitchCompat = itemView.findViewById(R.id.deviceSwitch)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder {
