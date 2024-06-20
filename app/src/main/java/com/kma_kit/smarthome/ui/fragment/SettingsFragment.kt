e86bcae9acee0a76cd2219e9299e578255d6f059
vu_nd <nguyendangvu221@gmail.com>
2024-06-20 17:32:04 +0700

    add navigation about me

--- app/src/main/java/com/kma_kit/smarthome/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/kma_kit/smarthome/ui/fragment/SettingsFragment.kt
@@ -1,17 +1,42 @@
 package com.kma_kit.smarthome.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.kma_kit.smarthome.R
+import com.kma_kit.smarthome.ui.activity.HomeScreenActivity
+import com.kma_kit.smarthome.ui.activity.LoginActivity
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        var view = inflater.inflate(R.layout.fragment_settings, container, false)
+        val aboutMeButton: CardView = view.findViewById(R.id.about_me_button)
+        val changeUsernameButton:CardView = view.findViewById(R.id.change_username_button)
+        val changePasswordButton: CardView = view.findViewById(R.id.change_password_button)
+        val logoutButton: CardView = view.findViewById(R.id.logout_button)
+
+        aboutMeButton.setOnClickListener {
+            var url: String = "https://smarthomekit.vn/"
+            if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                url = "https://$url"
+            }
+
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
+        }
+
+        logoutButton.setOnClickListener {
+            val intent = Intent(LoginActivity(),LoginActivity::class.java)
+            startActivity(intent)
+        }
+        return  view
     }
 }
